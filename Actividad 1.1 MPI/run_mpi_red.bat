cb5600467ad596e3cc0b870e9e63c5ceb60ac104
yolothestark <yolothestark@gmail.com>
2026-09-20 19:51:05 -0600

    Add network simulation files

---
+++ Actividad 1.1 MPI/run_mpi_red.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+title Simulacion Red MPI (3 Nodos)
+set PATH=%PATH%;C:\Program Files\CodeBlocks\MinGW\bin
+cd bin\Debug
+mpiexec -machinefile ..\hosts.txt -n 6 Actividad_1.exe
+echo.
+pause
